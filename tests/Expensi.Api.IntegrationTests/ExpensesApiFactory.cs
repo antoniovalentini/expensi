4d56a510b6cf685ab3d1346ea424dff560ddf4b1
antoniovalentini <antonio.valentini@live.it>
2025-05-04 23:20:54 +0200

    test db config

--- tests/Expensi.Api.IntegrationTests/ExpensesApiFactory.cs
+++ tests/Expensi.Api.IntegrationTests/ExpensesApiFactory.cs
@@ -3,6 +3,7 @@ using Expensi.Application;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 
@@ -13,6 +14,17 @@ public class ExpensesApiFactory : WebApplicationFactory<Program>
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
         builder.UseEnvironment("Development");
+
+        builder.ConfigureAppConfiguration((_, config) =>
+        {
+            config.AddInMemoryCollection([
+                new KeyValuePair<string, string?>("PostgresConnection:Host", "localhost"),
+                new KeyValuePair<string, string?>("PostgresConnection:Port", "5432"),
+                new KeyValuePair<string, string?>("PostgresConnection:Database", "test_db"),
+                new KeyValuePair<string, string?>("PostgresConnection:Username", "test_user"),
+                new KeyValuePair<string, string?>("PostgresConnection:Password", "test_password"),
+            ]);
+        });
 
         builder.ConfigureTestServices(services =>
         {
