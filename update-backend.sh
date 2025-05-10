7550051ea0647e3a0bcf52e906df55457c7c2f32
antoniovalentini <antonio.valentini@live.it>
2025-05-10 23:42:23 +0200

    New deploy script

---
+++ update-backend.sh
@@ -0,0 +1,24 @@
+﻿#!/bin/bash
+set -e
+
+echo "Attempting to stop Expensi backend container (if it exists)..."
+docker stop expensi-backend || true
+echo "Attempting to remove Expensi backend container (if it exists)..."
+docker rm expensi-backend || true
+echo "Pulling the latest Expensi image..."
+docker pull avdockie/expensi-backend:latest
+echo "Starting new Expensi backend container..."
+docker run \
+  -d -p 5008:5008 \
+  --name=expensi-backend \
+  --restart=always \
+  -e ASPNETCORE_URLS="http://*:5008" \
+  -e PostgresConnection__Host="$EXPENSI_HOST" \
+  -e PostgresConnection__Database="$EXPENSI_DB" \
+  -e PostgresConnection__Username="$FLYWAY_USER" \
+  -e PostgresConnection__Password="$FLYWAY_PASSWORD" \
+  --network="expensi_default" \
+  avdockie/expensi-backend:latest
+echo ""
+echo "Expensi backend deployment/update complete."
+exit 0
